77e3114a54392919826af3749f77f97a6da7e32e
Gokul Nathan <gokuln@verizonmedia.com>
2020-05-10 22:36:52 -0400

    changes

--- dgraph/zero/start.sh
+++ dgraph/zero/start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -ex
+replicas=$3
+srvName=$2
+podName=$1
 [[ `hostname` =~ -([0-9]+)$ ]] || exit 1
 ordinal=${BASH_REMATCH[1]}
 idx=$(($ordinal + 1))
 if [[ $ordinal -eq 0 ]]; then
-  exec dgraph zero --my=$(hostname -f):5080 --idx $idx --replicas 3
+  exec dgraph zero --my=$(hostname -f):5080 --idx $idx --replicas $replicas
 else
-  exec dgraph zero --my=$(hostname -f):5080 --peer dgraph-zero-0.dgraph-zero.${POD_NAMESPACE}.svc.cluster.local:5080 --idx $idx --replicas 3
+  exec dgraph zero --my=$(hostname -f):5080 --peer $podName.$srvName.${POD_NAMESPACE}.svc.cluster.local:5080 --idx $idx --replicas $replicas
 fi
